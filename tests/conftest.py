3eabbf906fae5cf7cf3461a114567553e42d50d6
demokrat86 <demokrat0831@gmail.com>
2025-10-23 22:12:54 +0300

    исправил замечания

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,13 @@ import help
 from data import Endpoints
 from help import User
 
+
 @pytest.fixture
 def user_registration_and_login():
     created_user = requests.post(f'{Endpoints.USER_REGISTER_URL}', data=User.valid_user)
     login_user = requests.post(f'{Endpoints.USER_LOGIN_URL}', data=User.valid_user)
-    return created_user, login_user  # Заменили yield на return
+    yield created_user, login_user
+
 
 @pytest.fixture
 def user_full_cycle(user_registration_and_login):
